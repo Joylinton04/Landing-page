e3ba957cf807b27a5ebe672b92d0bbc16e82ab89
JoyLinton04 <adujoy05@gmail.com>
2023-10-10 22:41:25 +0000

    Fixing a bug

--- src/layout/LandingPage.jsx
+++ src/layout/LandingPage.jsx
@@ -1,6 +1,6 @@
 import Navbar from "../components/header/Navbar";
 import HeaderImg from "../components/header/HeaderImg";
-import HomeScreen from "../components/HomeImg/homeScreen";
+import HomeScreen from "../components/HomeImg/HomeScreen";
 import Feature from "../components/Features/Feature";
 import ProductPlan from "../components/plan/ProductPlan";
 import Comments from "../components/comments/Comments";
